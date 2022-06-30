7a7a4753632ae4253de6f71c8ead6789b6ab6b6f
GitOffMyCode <52583811+GitOffMyCode@users.noreply.github.com>
2022-06-30 14:55:58 +0100

    updates

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,13 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+router.post("/CTAP-check-identity", function (req, res) {
+  var answer = req.session.data["CTAP-check-identity"];
+  if (answer === "yes") {
+    res.redirect("Option3/CTAP-test-information");
+  }
+  if (answer === "no") {
+    res.redirect("Option3/CTAP-barcode-lookup-blank");
+  }
+});
